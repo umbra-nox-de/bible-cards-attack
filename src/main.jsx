688aab887bf62e6e1e024106ec6d4c9e5f7a270a
umbra-nox-de <nhsk64hfdf@privaterelay.appleid.com>
2026-09-09 16:04:17 -0400

    Update 0.7: activate new interface

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App07";
 import "./styles.css";
+import "./update07.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
