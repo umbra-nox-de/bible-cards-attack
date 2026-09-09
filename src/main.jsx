b09aeb8f0cedd30a332148db8519b7f114ec9edf
umbra-nox-de <nhsk64hfdf@privaterelay.appleid.com>
2026-09-09 16:03:54 -0400

    Update 0.7: switch app to new battle systems

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App07";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
